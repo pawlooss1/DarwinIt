9540d3fb7dab65aa99e48c3770dc733a0f2a4d62
pawlooss1 <pawlooss13@gmail.com>
2019-01-18 16:00:46 +0100

    +getAllDirections

--- src/agh/cs/projekt2/MoveDirection.java
+++ src/agh/cs/projekt2/MoveDirection.java
@@ -1,5 +1,6 @@
 package agh.cs.projekt2;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public enum MoveDirection {
@@ -12,5 +13,16 @@ public enum MoveDirection {
     BACKWARD_RIGHT,
     BACKWARD_LEFT;
 
-    public List<MoveDirection>
+    public static List<MoveDirection> getAllDirections() {
+        List<MoveDirection> result = new LinkedList<>();
+        result.add(MoveDirection.FORWARD_LEFT);
+        result.add(MoveDirection.FORWARD);
+        result.add(MoveDirection.FORWARD_RIGHT);
+        result.add(MoveDirection.RIGHT);
+        result.add(MoveDirection.BACKWARD_RIGHT);
+        result.add(MoveDirection.BACKWARD);
+        result.add(MoveDirection.BACKWARD_LEFT);
+        result.add(MoveDirection.LEFT);
+        return result;
+    }
 }
